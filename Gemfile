b7e7bc48992dacc57d97d645f8e8130ed39b3300
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-16 16:25:11 +0100

    Add tests

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.7.5'
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
+gem 'ffi'
+
+gem 'rails-controller-testing'
+
 group :development, :test do
   gem 'rspec-rails', '~> 5.0.0'
 end
---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'User', type: :request do
+  describe 'GET #index' do
+    before(:example) { get('/user') }
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
+    end
+
+    it 'has a text in the view' do
+      expect(response.body).to include('Game of thrones')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get '/user/:id' }
+
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it 'has a text in the view' do
+      expect(response.body).to include('Harry Potter')
+    end
+  end
+end
