bef24bf05ce1ef2a376a2dc4cb3057e7ca44575a
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-22 09:51:13 +0100

    Create methods for like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "users_id"
   belongs_to :post, class_name: "Post", foreign_key: "posts_id"
-  # def update_likes
-  #   post.increment!(:likes_counter)
-  # end
+  
+  def update_likes
+    post.increment!(:likes_counter)
+  end
 end
