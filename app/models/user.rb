7d0273399ae262cdc7031bc313f6d508ec6313c8
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-22 09:58:20 +0100

    Create methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   has_many :comments, class_name: "Comment", foreign_key: "posts_id"
   has_many :likes, class_name: "Like", foreign_key: "posts_id"
 
-
+  def three_recent_posts
+    post.order(created_at: :desc).limit(3)
+  end
 end
