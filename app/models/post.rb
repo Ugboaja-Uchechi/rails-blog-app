01b09cb6c803c8fcd36d383d50e75fe72d9ae32b
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-22 10:06:04 +0100

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: "users_id"
-  has_many :comments, class_name: "Comment", foreign_key: "posts_id"
-  has_many :likes, class_name: "Like", foreign_key: "posts_id"
-  
+  belongs_to :author, class_name: 'User', foreign_key: 'users_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'posts_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'posts_id'
+
   def update_post_count
     users.increment!(:posts_counter)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, class_name: "Post", foreign_key: "users_id"
-  has_many :comments, class_name: "Comment", foreign_key: "posts_id"
-  has_many :likes, class_name: "Like", foreign_key: "posts_id"
+  has_many :posts, class_name: 'Post', foreign_key: 'users_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'posts_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'posts_id'
 
   def three_recent_posts
     post.order(created_at: :desc).limit(3)
