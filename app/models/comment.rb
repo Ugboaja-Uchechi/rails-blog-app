307824d7450faf1898180d689774ad6bf3c6e84d
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-02-28 08:41:55 +0100

    Write unit tests for models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'users_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'posts_id'
 
-  def self.update_comments_counter(id)
-    post = Post.find(id)
-    post.commentsCounter = Comment.where(posts_id: id).count
-    post.save
+  def .update_comments_counter
+    post.increment!(:comments_counter)
   end
 end
--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,10 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Likes', type: :request do
-  describe 'GET /_likes' do
-    it 'returns http success' do
-      get '/likes/_likes'
-      expect(response).to have_http_status(:success)
+  before(:each) do
+    @user = User.new(id: 1, name: 'Jim', bio: 'I am a teacher', photo: '', posts_counter: 0)
+    @post = Post.new(title: 'First Post', text: 'Hello', comments_counter: 0, likes_counter: 0, author_id: @user.id)
+    @like = Like.new(author_id: @user.id, post_id: @user.id)
+  end
+
+  describe 'validation tests' do
+    it 'validates the author_id is an integer' do
+      @like.author_id = 1
+      expect(@like.author_id).to eq(1)
+    end
+
+    it 'validates the author name' do
+      expect(@like.author.name).to eq('Jim')
+    end
+
+    it 'validates the post_id is an integer' do
+      @like.post_id = 1
+      expect(@like.post_id).to eq(1)
+    end
+
+    it 'validates the post title' do
+      expect(@like.post.title).to eq('First Post')
+    end
+  end
+
+  describe 'like model methods tests' do
+    it 'it should increment the post likes_counter' do
+      @post.save
+      @like.update_likes
+      expect(@post.likes_counter).to eq(1)
     end
   end
 end
