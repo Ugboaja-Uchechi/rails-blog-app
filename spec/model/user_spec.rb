71576cc6a752fd3924bf88c80917ea587c53b4ce
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-04 00:19:17 +0100

    Renove comments

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -44,25 +44,4 @@ RSpec.describe User, type: :model do
       expect(@user.three_recent_posts).to eq(@user.all_posts)
     end
   end
-  # subject { User.new(name: 'Tom', photo: '"https://www.goldenglobes.com/sites/default/files/styles/homepage_carousel/public/photo_gallery/jim_carrey_112204_lemony_snicket.jpg', bio: 'Teacher from Mexico.') }
-
-  # before { subject.save }
-
-  # it 'name should be present' do
-  #   subject.name = nil
-  #   expect(subject).to_not be_valid
-  # end
-
-  # it 'postsCounter should be greater than or equal to zero' do
-  #   subject.postsCounter = -1
-  #   expect(subject).to_not be_valid
-  # end
-
-  # it 'return the three latest posts for a user' do
-  #   expect(User.three_recent_posts(subject.id).count).to eql 0
-  # end
-
-  # it 'return all the posts for a user' do
-  #   expect(User.all_posts(subject.id).count).to eql 0
-  # end
 end
